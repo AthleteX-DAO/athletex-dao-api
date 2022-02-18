3f6976c5305d3ca24c825c4c18d7d4030b9d876c
Kevin Moturi <kevin@powermoves.io>
2022-02-18 14:20:00 -0800

    add release deploy workflows

---
+++ release_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "$SSH_PROJECT_FOLDER" || exit
+echo " * PULLING NEW IMAGES"
+git fetch
+git checkout release/1.0
+git pull
+docker-compose pull
+docker-compose stop
+echo " * CLEANING OLD IMAGES"
+docker-compose rm -f
+docker-compose up build
