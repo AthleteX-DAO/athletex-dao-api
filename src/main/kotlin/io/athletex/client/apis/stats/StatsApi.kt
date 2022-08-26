ca2905a84ff354a1758390362d0bfd0ce62c625f
alexblackwell1 <blackwellag@appstate.edu>
2022-08-26 19:29:00 -0400

    computeMLBPruce function copied

--- src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
+++ src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
@@ -37,7 +37,7 @@ suspend fun syncMlbStatsToDb(config: HoconApplicationConfig, mlbPlayerService: M
     println("MLB stats response size = ${playerStatsResponse.size}")
 
     var lgWeightOnBase = 0.0
-    var sumLeaguePlateAppearances = 0.0
+    var sumLeaguePlateAppearances = 0
     playerStatsResponse.filterNot { it.plateAppearances > 0 }.forEach {
         lgWeightOnBase += it.weightedOnBasePercentage
         sumLeaguePlateAppearances += it.plateAppearances
@@ -49,7 +49,8 @@ suspend fun syncMlbStatsToDb(config: HoconApplicationConfig, mlbPlayerService: M
     val statsUpdate = playerStatsResponse.map { player ->
         val name = player.name.removeNonSpacingMarks().replace(" ", "\'")
         val inningsPlayed = player.games * 9.0
-        val computedPrice = computePrice(player, lgWeightOnBase, sumLeaguePlateAppearances)
+        val computedPrice = computeMLBPrice(player, lgWeightOnBase, sumLeaguePlateAppearances)
+        // what is this copy doing?
         player.copy(
             name = name,
             inningsPlayed = inningsPlayed,
@@ -57,4 +58,70 @@ suspend fun syncMlbStatsToDb(config: HoconApplicationConfig, mlbPlayerService: M
         )
     } //TODO filter out non supported players
     mlbPlayerService.insertStatsUpdate(statsUpdate)
+
+    /* Thoughts (if I understand this somewhat....)
+    
+        statsUpdate: List<MLBPlayer>
+        check player name
+            - have mapping of used players
+        if valid player, add a copy of them to statsUpdate
+        push up statsUpdate
+     */
+}
+
+fun computeMLBPrice(athlete: MLBPlayer, lgWeightOnBase: Double, sumPlateAppearances: Int): Double {
+    // map position weights
+    val position_adj = mapOf(
+        "C" to 12.5,
+        "1B" to -12.5,
+        "2B" to 2.5,
+        "SS" to 7.5,
+        "3B" to 2.5,
+        "LF" to -7.5,
+        "CF" to 2.5,
+        "RF" to -7.5,
+        "DH" to -17.5
+    )
+    var avg50yrRPW = 9.757
+
+    // prepping calculation
+    var battingRuns = (athlete.plateAppearances * (athlete.weightedOnBasePercentage - lgWeightOnBase)) / 1.25
+    var baseRunningRuns = athlete.stolenBases * 0.2
+    var games = athlete.games * 9
+    var fieldingRuns
+    if (games <= 0) {
+        fieldingRuns = athlete.errors * -10 / games
+    } else {
+        fieldingRuns = 0
+    }
+    var positionAdjustment = ( athlete.games * 9 ) * position_adj[athlete.position] / 1458
+    var replacementRuns = ( athlete.plateAppearances * 5561.49 ) / sumPlateAppearances
+
+    // formula calculation
+    var statsNumerator = battingRuns + baseRunningRuns + fieldingRuns + positionAdjustment + replacementRuns
+    var war = statsNumerator / avg50yrRPW
+    var computedMajorLeagueBaseballPrice = war // * collateralizationMultiplier
+
+    return computedMajorLeagueBaseballPrice
+
+    /* --------THE PYTHON CODE--------
+            (athlete_data, lgweightedOnBase, sumPlateAppearances):
+
+        [x] BattingRuns = (((athlete_data['PlateAppearances']) * (athlete_data['WeightedOnBasePercentage'] - lgweightedOnBase)) / 1.25)
+        [x] BaseRunningRuns = (athlete_data['StolenBases'] * 0.2)
+        [x] FieldingRuns = 0
+        [x] games = (athlete_data['Games'] * 9 )
+        [x] FieldingRuns = ((athlete_data['Errors'] * (-10)) / games)
+        [x] if games <= 0:
+        [x]         FieldingRuns = 0
+        [x] PositonalAdjustment = (athlete_data['Games'] * 9 ) * position_adj.setdefault(athlete_data['Position'], 0) / 1458
+        [x] ReplacementRuns = (athlete_data['PlateAppearances'] * 5561.49) / sumPlateAppearances
+
+        # Formula Calculation
+        [x] statsNumerator = BattingRuns + BaseRunningRuns + FieldingRuns + PositonalAdjustment + ReplacementRuns
+        [x] WAR = statsNumerator / avg50yrRPW
+        [x] computedMajorLeagueBaseballPrice = WAR # * collateralizationMultiplier
+        [] return computedMajorLeagueBaseballPrice
+
+    */
 }
