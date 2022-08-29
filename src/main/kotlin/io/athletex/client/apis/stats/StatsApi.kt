dac5e01d6087eea0e0e3efc1c2e94ddcccc3667b
alexblackwell1 <blackwellag@appstate.edu>
2022-08-29 15:58:37 -0400

    NFL price function & (NFL) Player file added fields

--- src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
+++ src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
@@ -125,3 +125,31 @@ fun computeMLBPrice(athlete: MLBPlayer, lgWeightOnBase: Double, sumPlateAppearan
 
     */
 }
+
+fun computeNFLPrice(athlete: Player): Double {
+    
+    // Variables
+    val passingYards = athlete.passingYards / 25
+    val rushingYards = athlete.rushingYards / 10
+    val receivingYards = athlete.receivingYards / 10
+    val rushingTouchdowns = athlete.receivingTouchdowns * 6
+    val receivingTouchdowns = athlete.receivingTouchdowns * 6
+    val passTD = athlete.passingTouchdowns * 4
+    val reception = athlete.receptions * 0.5
+    val passingIntercept = athlete.passingInterceptions * -2
+    val fumblesLost = athlete.fumblesLost * -2
+
+    // Football Athletes
+    var numerator = passingYards + rushingYards + receivingYards + rushingTouchdowns + receivingTouchdowns + passTD + reception + passingIntercept + fumblesLost
+    var denominator = athlete.offensiveSnapsPlayed || athlete.defensiveSnapsPlayed
+    if (denominator == 0.0) {
+            denominator = 1.0
+    }
+
+    var computedAmericanFootballPrice = numerator / denominator
+    if (computedAmericanFootballPrice < 0) {
+            computedAmericanFootballPrice = 0
+    }
+
+    return computedAmericanFootballPrice
+}
--- src/main/kotlin/io/athletex/model/Player.kt
+++ src/main/kotlin/io/athletex/model/Player.kt
@@ -1,11 +1,28 @@
 package io.athletex.model
 
+import io.athletex.model.Player
 import kotlinx.serialization.Serializable
+import java.sql.ResultSet
 
 @Serializable
 abstract class Player {
-    abstract val id: Int
-    abstract val name: String
-    abstract val price: Double
-    abstract val timestamp: String
+    override val id: Int,
+    override val name: String,
+    val team: String,
+    val position: String,
+    
+    val passingYards: Double,
+    val rushingYards: Double,
+    val receivingYards: Double,
+    val rushingTouchdowns: Double,
+    val receivingTouchdowns: Double,
+    val passingTouchdowns: Double,
+    val receptions: Double,
+    val passingInterceptions: Double,
+    val fumblesLost: Double,
+    val offensiveSnapsPlayed: Double,
+    val defensiveSnapsPlayed: Double,
+
+    override val price: Double,
+    override val timestamp: String,
 }
