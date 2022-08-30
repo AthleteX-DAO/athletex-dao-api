896409d0c5c091b4c47c46b5e751c62a1fdf868d
alexblackwell1 <blackwellag@appstate.edu>
2022-08-30 13:48:10 -0400

    fix: int to double error

--- src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
+++ src/main/kotlin/io/athletex/client/apis/stats/StatsApi.kt
@@ -102,7 +102,7 @@ fun computeNFLPrice(athlete: Player): Double {
     val passingYards = athlete.passingYards / 25
     val rushingYards = athlete.rushingYards / 10
     val receivingYards = athlete.receivingYards / 10
-    val rushingTouchdowns = athlete.receivingTouchdowns * 6
+    val rushingTouchdowns = athlete.rushingTouchdowns * 6
     val receivingTouchdowns = athlete.receivingTouchdowns * 6
     val passTD = athlete.passingTouchdowns * 4
     val reception = athlete.receptions * 0.5
@@ -111,14 +111,17 @@ fun computeNFLPrice(athlete: Player): Double {
 
     // Football Athletes
     var numerator = passingYards + rushingYards + receivingYards + rushingTouchdowns + receivingTouchdowns + passTD + reception + passingIntercept + fumblesLost
-    var denominator = athlete.offensiveSnapsPlayed || athlete.defensiveSnapsPlayed
+    var denominator = athlete.offensiveSnapsPlayed
     if (denominator == 0.0) {
-            denominator = 1.0
+        denominator = athlete.defensiveSnapsPlayed
+    }
+    if (denominator == 0.0) {
+        denominator = 1.0
     }
 
     var computedAmericanFootballPrice = numerator / denominator
-    if (computedAmericanFootballPrice < 0) {
-            computedAmericanFootballPrice = 0
+    if (computedAmericanFootballPrice < 0.0) {
+            computedAmericanFootballPrice = 0.0
     }
 
     return computedAmericanFootballPrice
