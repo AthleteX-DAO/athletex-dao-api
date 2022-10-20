bc18d8b387c315755fdc1196c3d75ea6125026bd
Steven Zhivov <stevenzhivov1@gmail.com>
2022-10-20 14:58:23 -0400

    AX-1010 Added temp return value for NBA formula

---
+++ src/main/kotlin/io/athletex/client/formulas/NBACalculations.kt
@@ -0,0 +1,10 @@
+package io.athletex.client.formulas
+
+import io.athletex.client.apis.stats.models.BasketballFeedUpdateItem
+
+fun computeNBAPrice(
+    feedUpdateItem: BasketballFeedUpdateItem,
+): Double {
+    
+    return 0.0
+}
