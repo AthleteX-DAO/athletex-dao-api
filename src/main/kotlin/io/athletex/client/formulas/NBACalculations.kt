d215f6d49709b872fff25e884f66d95602dd27d3
Zurxae <59427397+Zurxae@users.noreply.github.com>
2022-11-01 13:31:37 -0400

    Ax 1010 nba backend create nba formula and add to formulas (#45)
    * Added NBA to enum class in AppConstants.kt
    
    * AX-1014 Added NBARoutes.kt and NBA to enum class in SportEnpoint.kt (has dependencies that are not finished yet.
    
    * AX-1011 BasketballPlayerInsertItem.kt will need more vals in the future
    
    * AX-1010 Added temp return value for NBA formula
    
    * AX-1010 Added real formula for nba token value
    
    Co-authored-by: alexblackwell1 <69765981+alexblackwell1@users.noreply.github.com>

--- src/main/kotlin/io/athletex/client/formulas/NBACalculations.kt
+++ src/main/kotlin/io/athletex/client/formulas/NBACalculations.kt
@@ -2,9 +2,36 @@ package io.athletex.client.formulas
 
 import io.athletex.client.apis.stats.models.BasketballFeedUpdateItem
 
-fun computeNBAPrice(
-    feedUpdateItem: BasketballFeedUpdateItem,
-): Double {
+val nbaStatMult = mapOf(
+    "Point" to 1,
+    "3PM" to 1,
+    "FGA" to -1,
+    "FGM" to 2,
+    "FTA" to -1,
+    "FTM" to 1,
+    "REB" to 1,
+    "AST" to 2,
+    "STL" to 4,
+    "BLK" to 4,
+    "TOV" to -2,
+)
+
+fun computeNBAPrice(feedUpdateItem: BasketballFeedUpdateItem): Double {
+    fantasyPoints = (((feedUpdateItem.points ?: 0.0) * nbaStatMult["Point"])
+                    +((feedUpdateItem.threePointersMade ?: 0.0) * nbaStatMult["3PM"])
+                    +((feedUpdateItem.fieldGoalsAttempted ?: 0.0) * nbaStatMult["FGA"])
+                    +((feedUpdateItem.fieldGoalsMade ?: 0.0) * nbaStatMult["FGM"])
+                    +((feedUpdateItem.freeThrowsAttempted ?: 0.0) * nbaStatMult["FTA"])
+                    +((feedUpdateItem.freeThrowsMade ?: 0.0) * nbaStatMult["FTM"])
+                    +((feedUpdateItem.rebounds ?: 0.0) * nbaStatMult["REB"])
+                    +((feedUpdateItem.assists ?: 0.0) * nbaStatMult["AST"])
+                    +((feedUpdateItem.steals ?: 0.0) * nbaStatMult["STL"])
+                    +((feedUpdateItem.blocks ?: 0.0) * nbaStatMult["BLK"])
+                    +((feedUpdateItem.turnovers ?: 0.0) * nbaStatMult["TOV"])
+    )
     
-    return 0.0
+    //fantasy points per minute normalized (* 100), for it to be between 0-1000AX
+    fppmN = (fantasyPoints / feedUpdateItem.minutesPlayed) * 100
+
+    return fppmN
 }
